0fb8e5cbd5430ac24085c2f74e98b0a472e46f73
asakasa <asakasa@me.com>
2015-02-28 23:29:16 +0900

    Update js/audio.js

--- js/audio.js
+++ js/audio.js
@@ -1,17 +1,12 @@
 /**
+ * This program is a utility for Audio API.
+ * This program NEEDs to load before
+ * a JavaScript file for main game is loaded.
+ */
+/**
  * Audio Context
  */
 var context;
-
-$(function(){
-  try{
-    context = new (window.AudioContext || window.webkitAudioContext)();
-    load_sounds();
-  }catch(e){
-    alert('Web Auid API is not supported');
-  }
-});
-
 /**
  * Set of Sound Resources
  * This includes a path to sound file and
@@ -26,9 +21,12 @@ $(function(){
 var sounds = {};
 /**
  * load_sounds - Loading some sounds and initializing them.
+ * @finished_func: callback function at load finished
  */
-function load_sounds(){
+function load_sounds(finished_func){
   $.getJSON("resources.json", function(data){
+    var cnt = 0;
+    const NUMBER_OF_SOUNDS = data.sounds.length;
     /**
      * Read some pieces of information about sound in JSON file.
      */
@@ -51,6 +49,9 @@ function load_sounds(){
         r.onload = function(){
           context.decodeAudioData(r.response, function(buf){
             sound.buffer = buf;
+            if(++cnt >= NUMBER_OF_SOUNDS){
+              finished_func();
+            }
           });
         };
         r.send();
@@ -60,28 +61,47 @@ function load_sounds(){
 }
 
 /**
- * play_sound - Play a sound.
- * @id                : sound identifier
- * @when    (optional): a pause time until start.
- * @offset  (optional): start time of the sound.
- * @duration(optional): a number of seconds that be played.
+ * build_sound - Build a sound source.
+ * @loop  (optional): sound loop flag( defaults to false).
+ * @volume(optional): sound volume( defaults to 1).
+ * @func  (optional): callback function at the end of play.
  *
- * @when and @offset, @duration parameters are a unit of seconds.
- * @when and @offset parameters default to 0.
+ * This function MUST be called before
+ * play_sound() functioan is called.
  */
-function play_sound(id, when, offset, duration){
-  when = when === undefined? 0: when;
-  offset = offset === undefined? 0: offset;
+function build_sound(id, loop, volume, func){
+  loop   = loop   === undefined? false: loop;
+  volume = volume === undefined?     1: volume;
+  func   = func   === undefined?  null: func;
 
   var s = context.createBufferSource();
   s.buffer = sounds[id].buffer;
-  s.connect(context.destination);
+  s.loop = loop;
+  s.onended = func;
   sounds[id].source = s;
+  var g = context.createGain();
+  g.gain.value = volume;
+
+  s.connect(g);
+  g.connect(context.destination);
+}
+/**
+ * play_sound - Play a sound.
+ * @id                : sound identifier.
+ * @when    (optional): a pause time until start( defaults to 0).
+ * @offset  (optional): start time of the sound( defaults to 0).
+ * @duration(optional): a number of seconds that be played.
+ *
+ * @when and @offset, @duration parameters are a unit of seconds.
+ */
+function play_sound(id, when, offset, duration){
+  when   = when   === undefined?     0: when;
+  offset = offset === undefined?     0: offset;
 
   if(duration === undefined)
-    s.start(when, offset);
+    sounds[id].source.start(context.currentTime + when, offset);
   else
-    s.start(when, offset, duration);
+    sounds[id].source.start(context.currentTime + when, offset, duration);
 }
 /**
  * stop_sound - Stop a sound.
